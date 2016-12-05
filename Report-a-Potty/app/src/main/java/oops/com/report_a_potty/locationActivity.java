80d40b2f5eec1eea066a0c27f431ddbfffd346b0
Vijay Thakkar <vijaythakkar@icloud.com>
2016-12-05 15:28:52 -0500

    placeholder  commit for the new mothod in location activity

--- Report-a-Potty/app/src/main/java/oops/com/report_a_potty/locationActivity.java
+++ Report-a-Potty/app/src/main/java/oops/com/report_a_potty/locationActivity.java
@@ -1,6 +1,8 @@
 package oops.com.report_a_potty;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -9,6 +11,14 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.vision.barcode.Barcode;
+import android.location.Geocoder;
+import java.util.List;
+import java.util.Locale;
+
+
+
+
 
 public class locationActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,14 +49,47 @@ public class locationActivity extends FragmentActivity implements OnMapReadyCall
     @Override
     public void onMapReady(GoogleMap googleMap)
     {
+        // we make the map variable first
         mMap = googleMap;
 
+        // first we get the string address from the enter address activity
         Intent intent = getIntent();
-        final String message = intent.getStringExtra(enterAddressActivity.EXTRA_MESSAGE);
+        final String currentStringAddress = intent.getStringExtra(enterAddressActivity.EXTRA_MESSAGE);
+
+        // now use geocoder to get the LatLng
+        LatLng currentLatLngAddress = getCoordinatesFromAddress(Context.CONTEXT_IGNORE_SECURITY, currentStringAddress);
+
+
+
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
+
+    // we make a wrapper function for getting the geocoded LatLng from the string address
+    public LatLng getCoordinatesFromAddress(Context appContext, String addressString) {
+        LatLng outputLatLng = null;
+        Geocoder geocoder = new Geocoder(appContext, Locale.getDefault());
+        List< Address > geocoderResults = null;
+        try {
+            // get results form geocoder first
+            geocoderResults = geocoder.getFromLocationName(addressString, 1);
+            while (geocoderResults.size()==0) {
+                geocoderResults = geocoder.getFromLocationName(addressString, 1);
+            }
+            if (geocoderResults.size()>0) {
+                Address address = geocoderResults.get(0);
+                outputLatLng = new LatLng(address.getLatitude(),address.getLongitude());
+            }
+        } // look for any excpetions
+        catch (Exception except) {
+            System.out.print(except.getMessage());
+        }
+        // return that latitude and longitude
+        return outputLatLng;
     }
 }
