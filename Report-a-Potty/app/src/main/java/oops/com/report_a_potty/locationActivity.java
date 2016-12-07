b83f350bb878bad70cf72bd705efdb0ae5180e67
Vijay Thakkar <vijaythakkar@icloud.com>
2016-12-07 18:07:42 -0500

    Added framework for GPS location pulling

--- Report-a-Potty/app/src/main/java/oops/com/report_a_potty/locationActivity.java
+++ Report-a-Potty/app/src/main/java/oops/com/report_a_potty/locationActivity.java
@@ -1,8 +1,11 @@
 package oops.com.report_a_potty;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Address;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -13,6 +16,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.vision.barcode.Barcode;
 import android.location.Geocoder;
+import android.support.v7.app.AlertDialog;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
@@ -64,14 +70,18 @@ public class locationActivity extends FragmentActivity implements OnMapReadyCall
         // Add a marker at your location and move the camera to that location
         mMap.addMarker(new MarkerOptions().position(currentLatLngAddress).title("You Are Here"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLngAddress));
+        mMap.moveCamera(CameraUpdateFactory.zoomIn());
     }
 
-    public LatLng buttonDecision(char buttonCode, Context appCont, String currentStringAddress) {
+    public LatLng buttonDecision(char buttonCode, Context appContext, String currentStringAddress) {
         if (buttonCode == 'A') {
             // Get the coordinates from the string entered in the enter address activity
-            LatLng currentLatLngAddress = getCoordinatesFromAddress(appCont, currentStringAddress);
+            LatLng currentLatLngAddress = getCoordinatesFromAddress(appContext, currentStringAddress);
             return currentLatLngAddress;
-        //} else {
+        } else {
+            // first check if  the GPS is alright to use or not
+            final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            checkGPSStatus(locationManager, appContext);
             // Pass to GPS function (not written yet)
             //LatLng currentLatLngAddress = getCoordinatesFromGPS(appCont, currentStringAddress);
             //return currentLatLngAddress;
@@ -89,18 +99,54 @@ public class locationActivity extends FragmentActivity implements OnMapReadyCall
         try {
             // get results form geocoder first
             geocoderResults = geocoder.getFromLocationName(addressString, 1);
-            while (geocoderResults.size()==0) {
+            int counter = 0;
+            while (geocoderResults.size()==0 && counter < 3) {
                 geocoderResults = geocoder.getFromLocationName(addressString, 1);
+                counter++;
             }
             if (geocoderResults.size()>0) {
                 Address address = geocoderResults.get(0);
-                outputLatLng = new LatLng(address.getLatitude(),address.getLongitude());
+                outputLatLng = new LatLng(address.getLatitude(), address.getLongitude());
             }
+            /*
+            else {
+                String provider = LocationManager.GPS_PROVIDER;
+                outputLatLng = new LatLng(LocationManager.getLastKnownLocation(provider));
+            }
+            */
         } // look for any exceptions
         catch (Exception except) {
             System.out.print(except.getMessage());
         }
         // return that latitude and longitude
         return outputLatLng;
+    }
+
+    // user might have location services disabled so we first check for that
+    // first we get the location manager
+    public void checkGPSStatus(LocationManager locationManager, Context appContext){
+        if (! locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            alertNoGPS();
+        }
+    }
+
+    private void alertNoGPS() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Location services are disabled. Do you wish to enable them?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                            startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                            dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
     }
 }
